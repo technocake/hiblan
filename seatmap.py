b824785da8c7ed9a2ecf0e47a71ff2e2f19a50de
Robin G. Aaberg <robin.garen@gmail.com>
2012-04-13 22:54:12 +0200

    Color disco

--- seatmap.py
+++ seatmap.py
@@ -22,20 +22,25 @@ LOGO = """
 """
 
 def make_panel(h,l, y,x, str): 
+	curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+
 	win = curses.newwin(h,l, y,x) 
 	win.erase() 
 	#win.box() 
-	win.addstr(2, 2, str) 
+	#win.addstr(2, 2, str, curses.color_pair(1))
 	panel = curses.panel.new_panel(win) 
 	return win, panel 
 
 def seatmap(win):
-	
-	win.addstr(0,0, str(win.getmaxyx()))
+
+	(MY, MX) = win.getmaxyx() #putting screen size in a corner
 	win.border(0)
+	win.addstr(0,0, str(win.getmaxyx()))
+	win.addstr(MY-1, MX/2-4, "Colormode: %s" % curses.has_colors() ) #Tells if we can use colors or not :)
+
 	win.refresh()
 
-	(MY, MX) = win.getmaxyx()
+	
 
 	(logowin, logopan) = make_panel(20, 100, MY/2-20/2, MX/2-100/2, LOGO)
 	
@@ -43,7 +48,7 @@ def seatmap(win):
 	logopan.top()
 	logowin.refresh()	
 
-
+	
 
 
 	while 1:
@@ -51,8 +56,11 @@ def seatmap(win):
 		(PY,PX) = logowin.getmaxyx()
 		(MY, MX) = win.getmaxyx()
 
-		Y,X = random.randrange(0, MY+1-PY), random.randrange(0, MX+1-PX) 
+		Y,X = random.randrange(0, MY-PY), random.randrange(0, MX+1-PX) 
  		logopan.move(Y,X)
+ 		curses.init_pair(1,random.randrange(0, 8), 0 )
+ 		logowin.addstr(2, 2, LOGO, curses.color_pair(1))
+
 		curses.panel.update_panels()
 		logowin.refresh()
 		win.refresh()

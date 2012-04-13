3f6fe193d2cbbab727ec3c362be8780ce61f55aa
Robin G. Aaberg <robin.garen@gmail.com>
2012-04-13 22:38:56 +0200

    HIBLAN ascii demo :D

--- seatmap.py
+++ seatmap.py
@@ -7,6 +7,7 @@ import locale
 locale.setlocale(locale.LC_ALL, '')
 code = locale.getpreferredencoding()
 import time
+import random
 
 
 LOGO = """
@@ -23,7 +24,7 @@ LOGO = """
 def make_panel(h,l, y,x, str): 
 	win = curses.newwin(h,l, y,x) 
 	win.erase() 
-	win.box() 
+	#win.box() 
 	win.addstr(2, 2, str) 
 	panel = curses.panel.new_panel(win) 
 	return win, panel 
@@ -34,15 +35,27 @@ def seatmap(win):
 	win.border(0)
 	win.refresh()
 
-	(logowin, logopan) = make_panel(20, 150, 30, 20, LOGO)
+	(MY, MX) = win.getmaxyx()
+
+	(logowin, logopan) = make_panel(20, 100, MY/2-20/2, MX/2-100/2, LOGO)
 	
 	curses.panel.update_panels()
 	logopan.top()
 	logowin.refresh()	
 
 
+
+
 	while 1:
-		time.sleep(10)
+		time.sleep(1) 
+		(PY,PX) = logowin.getmaxyx()
+		(MY, MX) = win.getmaxyx()
+
+		Y,X = random.randrange(0, MY+1-PY), random.randrange(0, MX+1-PX) 
+ 		logopan.move(Y,X)
+		curses.panel.update_panels()
+		logowin.refresh()
+		win.refresh()
 		#CMD = win.getch()
 		
 	#raise Exception

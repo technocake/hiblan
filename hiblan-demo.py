14fa3ef160a1f320930c6f08732210dc6315010c
Robin G. Aaberg <robin.garen@gmail.com>
2012-04-15 21:25:21 +0200

    Fixed the bug that made the logo not appear in the right part of the screen.

--- hiblan-demo.py
+++ hiblan-demo.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python
 # coding: utf-8
 
-import curses
-import curses.panel
-import locale
-locale.setlocale(locale.LC_ALL, '')
-code = locale.getpreferredencoding()
-import time
-import random
-
+import curses, curses.panel, time, random
 
 LOGO = """
  88          88 88          88                         
@@ -26,8 +19,6 @@ def make_panel(h,l, y,x, str):
 
 	win = curses.newwin(h,l, y,x) 
 	win.erase() 
-	#win.box() 
-	#win.addstr(2, 2, str, curses.color_pair(1))
 	panel = curses.panel.new_panel(win) 
 	return win, panel 
 
@@ -40,9 +31,9 @@ def seatmap(win):
 
 	win.refresh()
 
-	
+	logolen = len(LOGO.split('\n')[1])+1 #Width of the logo
 
-	(logowin, logopan) = make_panel(20, 100, MY/2-20/2, MX/2-100/2, LOGO)
+	(logowin, logopan) = make_panel(20, logolen, MY/2-20/2, MX/2-100/2, LOGO)
 	
 	curses.panel.update_panels()
 	logopan.top()
@@ -55,7 +46,7 @@ def seatmap(win):
 		time.sleep(1) 
 		(PY,PX) = logowin.getmaxyx()
 		(MY, MX) = win.getmaxyx()
-
+		win.addstr(0, MX/2, str(logowin.getmaxyx()) )
 		Y,X = random.randrange(0, MY-PY), random.randrange(0, MX+1-PX) 
  		logopan.move(Y,X)
  		curses.init_pair(1,random.randrange(0, 8), 0 )
@@ -64,12 +55,5 @@ def seatmap(win):
 		curses.panel.update_panels()
 		logowin.refresh()
 		win.refresh()
-		#CMD = win.getch()
-		
-	#raise Exception
-
 
 curses.wrapper(seatmap)
-
-
-#print curses.__file__
